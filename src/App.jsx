ad8514924af34ecb3a27e823e44151b977ea268d
5Ankit585 <virendarrawat884@gmail.com>
2025-08-01 15:43:29 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,34 @@
-// src/App.jsx
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import './index.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Colleges from './pages/Colleges';
 import Exams from './pages/Exams';
 import Scholarships from './pages/Scholarships';
 import QAPage from './pages/QAPage';
-import CourseRecommender from './pages/CourseRecommender';
-import CollegePage from './pages/CollegePage';
 import StudentRegister from './pages/StudentRegister';
 import UniversityRegister from './pages/UniversityRegister';
+import CourseRecommender from './pages/CourseRecommender';
+import CollegePage from './pages/CollegePage';
+import RegistrationPage from './pages/university/RegistrationPage';
+import Navbar from './components/Navbar';
 
-const App = () => {
+function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      {/* Navbar always on top */}
+    <Router>
       <Navbar />
-
-      {/* Main Content Area */}
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/colleges" element={<Colleges />} />
-          <Route path="/exams" element={<Exams />} />
-          <Route path="/scholarships" element={<Scholarships />} />
-          <Route path="/qa" element={<QAPage />} />
-          <Route path="/recommend" element={<CourseRecommender />} />
-          <Route path="/college/:id" element={<CollegePage />} />
-          <Route path="/student-register" element={<StudentRegister />} />
-          <Route path="/university-register" element={<UniversityRegister />} />
-
-          {/* Optional: 404 Page */}
-          <Route path="*" element={<div className="p-6 text-center text-red-600">Page Not Found</div>} />
-        </Routes>
-      </main>
-
-      {/* Footer always at bottom */}
-      <Footer />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/colleges" element={<Colleges />} />
+        <Route path="/exams" element={<Exams />} />
+        <Route path="/scholarships" element={<Scholarships />} />
+        <Route path="/qa" element={<QAPage />} />
+        <Route path="/student-register" element={<StudentRegister />} />
+        <Route path="/university-register" element={<UniversityRegister />} />
+        <Route path="/course-recommender" element={<CourseRecommender />} />
+        <Route path="/college/:id" element={<CollegePage />} />
+        <Route path="/university/registration" element={<RegistrationPage />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
