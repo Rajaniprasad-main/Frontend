9756fce629d7c92130853b11ce5b6ed7cbda1c83
rajani-prasad <prasadrajani80@gmail.com>
2025-10-15 18:09:55 +0530

    Perf: optimized Vite chunk splitting and increased warning limit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins:[react(),
-  tailwindcss(),
-  ]
-})
+  plugins: [react(), tailwindcss()],
+
+  build: {
+    // Optional: increase chunk size warning limit
+    chunkSizeWarningLimit: 1000,
+
+    rollupOptions: {
+      output: {
+        // Split vendor (library) code into separate chunks
+        manualChunks: {
+          react: ["react", "react-dom", "react-router-dom"],
+          icons: ["react-icons"],
+          ui: ["framer-motion", "lucide-react"],
+        },
+      },
+    },
+  },
+});
